ea07a6f5eab1a9d209f9195c0c663b373cb7b080
Egert Gjyla <egertgjyla1995@gmail.com>
2018-03-10 15:55:49 +0100

    Sketched the list Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class Input extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 offset-md-3">
-            <div className="input-group mb-3">
-              <input type="text" className="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1"/>
+      <div className="jumbotron">
+        <h1 className="mb-3">To-Do List</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1"/>
+              </div>
             </div>
           </div>
         </div>
@@ -17,12 +22,41 @@ class Input extends Component {
   }
 }
 
+class List extends Component {
+  render() {
+    
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+          <ul class="list-group">
+            {this.props.tasks.map(task =>
+              <li className="list-group">{task}</li>
+            )}
+          </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tasks: [
+        'Task 1',
+        'Task 2'
+      ]
+
+    }
+  }
   render() {
     return (
-      <div className="App jumbotron">
-        <h1>To-Do List</h1>
+      <div className="App">
         <Input />
+        <List tasks={this.state.tasks} />
       </div>
     );
   }
